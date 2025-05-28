ece601c2ef7d6c07cf768804ea21222b03a0613f
kaimu-r <kaimu.seino@rizapgroup.com>
2025-05-28 16:35:10 +0900

    #7 ファイルの読み込み用のメソッドとcsvファイルの書き込み用のメソッドを追加

--- file_wrapper.rb
+++ file_wrapper.rb
@@ -1,4 +1,5 @@
 require "json"
+require "csv"
 
 # ファイル操作をラップするクラス
 class FileWrapper
@@ -18,6 +19,23 @@ class FileWrapper
         # 指定モードでファイルを開き、Fileオブジェクトをインスタンス変数に保持
         @file = File.open(file_path, mode)
     end
+
+    def read
+        # ファイル内容を取得。stringを返す
+        self.file.read
+    end
+
+    def read_json()
+        str = self.read
+        if str.empty?
+            str = {}
+        else
+            str = JSON.parse(str)
+        end
+
+        # hashを返す
+        str
+    end
     
     # 内容を保存
     def write(str)
@@ -29,5 +47,24 @@ class FileWrapper
     def write_json(hash)
         # ファイルを書き込んで返す
         self.write(JSON.pretty_generate(hash))
+    end
+
+    # Arrayを文字列に変換・改行コードを入れてcsv書き込む
+    def write_csv(rows, headers: nil)
+        # csvファイルに書き込む用の改行コードを記述した文字列を定義
+        csv_string = CSV.generate do |csv|
+            # ヘッダーが存在する場合は一行目に追加
+            if headers
+                csv.add_row(headers)
+            end
+
+            # 配列を一行ずつ取得していく
+            rows.each do |row|
+                csv.add_row(row)
+            end
+        end
+
+        # ファイルを書き込んで返す
+        write(csv_string)
     end
 end
