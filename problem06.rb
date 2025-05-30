26441235154e38c3e203970e194abdd3af0fac9c
kaimu-r <kaimu.seino@rizapgroup.com>
2025-05-30 12:03:57 +0900

    #11 csvファイルのデータを読み込んでユーザーが未成年かどうかをチェックする。

--- problem06.rb
+++ problem06.rb
@@ -1,8 +1,47 @@
 require './user.rb'
+require './csv_wrapper.rb'
 
-begin
-  user = User.new("Taro", "東京都", "新宿区", nil, nil, nil, "2015/01/01")
-  puts "登録完了！"
-rescue User::UnderageError => e
-  puts e.message
+# CSVファイルのパスを指定
+file_path = 'data/personal_infomation (3).csv'
+
+# 各列の命名規則を定義
+HEADER_MAPPING = {
+    'no' => 'id',
+    'namae' => 'name',
+    'rubi' => 'ruby',
+    'seibetu' => 'sex',
+    'denwa' => 'tel',
+    'keitai' => 'mobile',
+    'mairu' => 'mail',
+    'jusho1' => 'address1',
+    'jusho2' => 'address2',
+    'jusho3' => 'address3',
+    'jusho4' => 'address4',
+    'jusho5' => 'address5',
+    'tanjobi' => 'birthday'
+}
+
+# CSVWrapperクラスのインスタンスを生成し、ファイルを読み込む
+csv_wrapper = CSVWrapper.new(file_path)
+
+# ヘッダーを変換して各行をハッシュに変換
+row_data = csv_wrapper.headers_converter(HEADER_MAPPING)
+
+row_data.each do |row|
+  begin
+    # Userクラスのインスタンスを生成
+    user = User.new(
+        row['name'],
+        row['address1'],
+        row['address2'],
+        row['address3'],
+        row['address4'],
+        row['address5'],
+        row['birthday']
+    )
+
+    puts user.name
+  rescue User::UnderageError => e
+    puts e.message
+  end
 end
